397e1e1fb08c8270921a7344789e58deda1c95fa
umbra <julek.zalewski@gmail.com>
2025-05-21 17:20:32 +0200

    during labs

--- lab4/lab4_1.R
+++ lab4/lab4_1.R
@@ -2,8 +2,11 @@ rm(list = ls())
 
 alpha = 5e-2
 
+# a
 # H0: p_w=p_k
 # H1: p_w>p_k 
 prop.test(x=c(40, 31), n=c(233, 220), alternative="gr", conf.level=1-alpha)
 # we cannot reject H0
+# by default corrects using some magic Yates' continuity correction (see docs)
 
+# b
