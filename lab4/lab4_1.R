9fe388d0b9d92f9259fbed93b24ded63e9ebbac0
umbra <julek.zalewski@gmail.com>
2025-05-21 17:18:40 +0200

    during labs

--- lab4/lab4_1.R
+++ lab4/lab4_1.R
@@ -5,4 +5,4 @@ alpha = 5e-2
 # H0: p_w=p_k
 # H1: p_w>p_k 
 
-prop.test(x=c(40, 31), n=c(233, 220), alternative="t", conf.level=1-alpha)
+prop.test(x=c(40, 31), n=c(233, 220), alternative="gr", conf.level=1-alpha)
