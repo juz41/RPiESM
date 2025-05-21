5cb50cd1ea620e30ddad3db10d605a4e09359a66
umbra <julek.zalewski@gmail.com>
2025-05-21 17:15:39 +0200

    during labs

--- lab3/lab3_4.R
+++ lab3/lab3_4.R
@@ -5,4 +5,6 @@ alpha = 5e-2
 # H0: p = 0.8
 # H1: p != 0.8
 # we can use prop.test
-prop.test(x=118, n=150, p=8e-1, conf.level=1-alpha, alternative="t")
+prop.test(x=118, n=150, p=8e-1, alternative="t")
+
+# using binom.test shan't change the verdict
