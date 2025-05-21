388631fc7e219bb67807184c30ac5b60e676e884
umbra <julek.zalewski@gmail.com>
2025-05-21 16:59:11 +0200

    during labs

--- lab3/lab3_3.R
+++ lab3/lab3_3.R
@@ -4,7 +4,7 @@ alpha = 5e-2
 
 # a
 # restrictions from model iv sheet are met
-# 128 >= 5, 400-128 >= 5
+# 128 >= 5, 400-128 >= 5 -> we can use prop.test, not binom.test
 # H0: p = 0.35
 # H1: p < 0.35
-prop.test(128, 400, p=3.5e-1, alternative="less", conf.level=1-alpha, correct = TRUE)
+prop.test(x=128, n=400, p=3.5e-1, alternative="less", conf.level=1-alpha)
