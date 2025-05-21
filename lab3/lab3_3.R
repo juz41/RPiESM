9778dd376e93a703c860ef939bdf3c3b884b6d05
umbra <julek.zalewski@gmail.com>
2025-05-21 16:57:41 +0200

    during labs

--- lab3/lab3_3.R
+++ lab3/lab3_3.R
@@ -1,2 +1,10 @@
 rm(list = ls())
 
+alpha = 5e-2
+p = 3.5e-1
+
+# a
+# restrictions from model iv sheet are met
+# H0: p = 0.35
+# H1: p < 0.35
+prop.test(128, 400, p=p, alternative="less", conf.level=1-alpha, correct = TRUE)
