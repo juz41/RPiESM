63f4178a4145edbe8765ee1910b5f32c67864220
umbra <julek.zalewski@gmail.com>
2025-05-21 17:23:01 +0200

    during labs

--- lab4/lab4_1.R
+++ lab4/lab4_1.R
@@ -10,7 +10,7 @@ prop.test(x=c(40, 31), n=c(233, 220), alternative="gr", conf.level=1-alpha)
 # by default corrects using some magic Yates' continuity correction (see docs)
 
 # b i
-power.prop.test(n=c(233, 220), p1=1.7e-1, p2=1.4e-1)
+power.prop.test(n=c(233, 220), p1=1.7e-1, p2=1.4e-1, alternative="o")
 
 
 # b ii
