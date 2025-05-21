5b52ba26448c83094fad4062036a594be33f8255
umbra <julek.zalewski@gmail.com>
2025-05-21 16:23:35 +0200

    kozy init

---
+++ lab3/lab3_2.R
@@ -0,0 +1,12 @@
+# rm(list = ls())
+# commented out for goats were manually imported
+
+# goats from website
+# https://pages.mini.pw.edu.pl/~dembinskaa/www/?Dydaktyka%26nbsp%3B:Materia%C5%82y_dydaktyczne:Zbiory_danych
+# on top right - import dataset
+# functions to load files to check
+
+alpha = 5e-2
+
+# a
+interval = t.test(goats$WeightInitial, conf.level=1-alpha)$conf
