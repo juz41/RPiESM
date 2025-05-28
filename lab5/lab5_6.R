9b278eeb0e6b1ccad021bb1508623949cf89d117
umbra <julek.zalewski@gmail.com>
2025-05-28 17:45:06 +0200

    during labs

--- lab5/lab5_6.R
+++ lab5/lab5_6.R
@@ -1,6 +1,6 @@
 rm(list = ls())
 
-par("mfrow"=c(2,3))
+par("mfrow"=c(2,2))
 N = 100
 x1 = rnorm(n=N, 20, 5)
 x2 = runif(n=N, -1, 1)
@@ -17,7 +17,26 @@ qqnorm(x3, main="Exp")
 qqline(x3)
 qqnorm(x4, main="Pois")
 qqline(x4)
-qqnorm(x5, main="Cauchy")
-qqline(x5)
+# qqnorm(x5, main="Cauchy")
+# qqline(x5)
 
 # b
+boxplot(x1, main="Norm")
+boxplot(x2, main="Unif")
+boxplot(x3, main="Exp")
+boxplot(x4, main="Pois")
+
+# c
+hist(x1, main="Norm")
+hist(x2, main="Unif")
+hist(x3, main="Exp")
+hist(x4, main="Pois")
+
+# d
+alpha = 5e-2
+# H0: it is normal
+shapiro.test(x1)
+shapiro.test(x2)
+shapiro.test(x3)
+shapiro.test(x4)
+# for x1 p-value is rather enormous, so we cannot reject H0, in other cases we can
--- lab5/lab5_7.R
+++ lab5/lab5_7.R
@@ -1,2 +1,3 @@
 rm(list = ls())
 
+# additional if wanted hw
