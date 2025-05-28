972238dfa45830975785f9ec2f749c585f8ad062
umbra <julek.zalewski@gmail.com>
2025-05-28 17:05:34 +0200

    during labs

--- lab5/lab5_3.R
+++ lab5/lab5_3.R
@@ -1,8 +1,9 @@
 rm(list = ls())
 
-
 p = 5e-1
 n = 3
 data = c(24, 73, 77, 26)
 p_data = dbinom(0:3, size=n, prob=p)
 chisq.test(x=data, p=p_data)
+
+# p-value - we reject H0 and we doubt that he is gonna pass
