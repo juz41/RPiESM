df59596924c20ea5d124222d8efee390518d1381
umbra <julek.zalewski@gmail.com>
2025-05-28 16:31:49 +0200

    during labs

--- lab4/lab4_3.R
+++ lab4/lab4_3.R
@@ -10,5 +10,8 @@ after = hemoglobina$po
 # H0: mi_przed=mi_po
 # H1: mi_przed>mi_po
 t.test(before, after, alt="gr", paired=TRUE)
-# p-value < 0.05 => we can reject H0
+# p-value < 0.05 => we can reject H0 (drug works)
 
+power.t.test(n=length(before), delta=1.5, sd=sd(before-after), type="p", 
+             alt="o", sig.level=5e-2)
+# power is 0.9362774, so the diff of 1.5% is big and easily detectable
