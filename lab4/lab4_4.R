70fcbdbd0e4cc8cce1dc0e00e04425e9d6318711
umbra <julek.zalewski@gmail.com>
2025-05-28 16:35:49 +0200

    during labs

--- lab4/lab4_4.R
+++ lab4/lab4_4.R
@@ -2,8 +2,11 @@ rm(list = ls())
 
 library(MASS)
 # model 4, because we have info on normality
-nlschools
-iqs = nlschools$IQ
-sess = nlschools$SES
+# nlschools
+alpha = 5e-2
+x = nlschools$IQ
+y = nlschools$SES
+
 # H0: mi_1=mi_2
 # H1: mi_1mi_2
+# 1 - richer, 2 - poorer
