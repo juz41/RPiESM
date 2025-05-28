7af5e05a8a74add9231a53e684e16ca44c152af8
umbra <julek.zalewski@gmail.com>
2025-05-28 16:25:31 +0200

    during labs

--- lab4/lab4_3.R
+++ lab4/lab4_3.R
@@ -1,8 +1,13 @@
-rm(list = ls())
+# rm(list = ls())
 
 # getwd setwd
-hemoglobina = read.delim("~/Documents/coding/R/lab4/hemoglobina.txt",
-                         header = TRUE)
+# hemoglobina <- read.csv("~/Documents/coding/R/lab4/hemoglobina.txt", sep="")
 
 # not 1.5% but 1.5% pp
+before = hemoglobina$przed
+after = hemoglobina$po
 
+# H0: mi_przed=mi_po
+# H1: mi_przed>mi_po
+t.test(before, after, alt="gr", paired=TRUE)
+# p-value < 0.05 => 
