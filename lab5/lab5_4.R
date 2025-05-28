995cc1a8f4f05f39b91fc7a58dade781d7826cd2
umbra <julek.zalewski@gmail.com>
2025-05-28 17:18:32 +0200

    during labs

--- lab5/lab5_4.R
+++ lab5/lab5_4.R
@@ -6,7 +6,13 @@ data2 = rep(0:5, data)
 lambda_estimator <- fitdistr(data2,"Poisson")$estimate
 # or just calculate using mean
 
-p_data = c(dpois(0:4, lambda=lambda_estimator), 1-sum(dpois(0:4, lambda=lambda_estimator)))
+# p_data = c(dpois(0:4, lambda=lambda_estimator), 1-sum(dpois(0:4, lambda=lambda_estimator)))
+p_data = c(dpois(0:4, lambda=lambda_estimator), ppois(4, lambda_estimator, lower.tail=FALSE))
+
+sum(data)*p_data
+# we can use chisq.test
+
 chisq.test(x=data, p=p_data)
 
-# we cannot reject H0 (dist is Poisson)
+# p-value: 0.08306 < 0.1
+# we reject H0 (dist is not Poisson)
