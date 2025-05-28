f0c1f9a08aefa9e37f0330bb195ece0cfb6587f7
umbra <julek.zalewski@gmail.com>
2025-05-28 17:36:36 +0200

    during labs

--- lab5/lab5_5.R
+++ lab5/lab5_5.R
@@ -1,2 +1,14 @@
 rm(list = ls())
 
+time <- read.csv("~/Documents/coding/R/lab5/infolinia.txt", sep="")
+
+alpha = 5e-2
+a = 4.5
+beta = 4
+
+# H0: data is from gamma(a, beta)
+# H1: it is not
+
+ks.test(x=time, y="pgamma", alternative="t", shape=4.5, rate=4)
+# p-value = 0.3745 > alpha
+# we have no grounds to reject data is from gamma
--- lab5/lab5_6.R
+++ lab5/lab5_6.R
@@ -1,2 +1,11 @@
 rm(list = ls())
 
+par("mfrow"=c(2,2))
+N=100
+x1 = rnorm(n=N, 20, 5)
+x2 = runif(n=N, -1, 1)
+x3 = rexp(n=N, 5)
+x4 = rpois(n=N, 3)
+
+# a
+qqnorm(x)
