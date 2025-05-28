70c5ec8cc1ab65a419fa61758c009c9a73363192
umbra <julek.zalewski@gmail.com>
2025-05-28 16:44:21 +0200

    during labs

--- lab4/lab4_4.R
+++ lab4/lab4_4.R
@@ -4,9 +4,17 @@ library(MASS)
 # model 4, because we have info on normality
 # nlschools
 alpha = 5e-2
-x = nlschools$IQ
-y = nlschools$SES
+med = median(nlschools$SES)
+
+# x - richer, y - poorer
+x = nlschools$IQ[nlschools$SES>med]
+y = nlschools$IQ[nlschools$SES<=med]
+n1 = length(x)
+n2 = length(y)
 
 # H0: mi_1=mi_2
-# H1: mi_1mi_2
-# 1 - richer, 2 - poorer
+# H1: mi_1>mi_2
+U = (mean(x)-mean(y))/(sqrt(var(x)/n1+var(y)/n2))
+1 - pnorm(U) # p-value - probability of U > t
+pnorm(U, lower.tail=F)
+# 
