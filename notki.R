db5d514f77137830ee55d163395019e79a7ec6ea
juz41 <julek.zalewski@gmail.com>
2025-06-06 20:03:49 +0200

    kolos prep

--- notki.R
+++ notki.R
@@ -12,10 +12,24 @@ can_reject <- function(pvalue, alpha) {
 
 
 # kol1
-
-
 # 1
 data = c(20.4, 24.2, 18.6, 19.6, 23.0, 21.8)
 sigma.test(data, conf.level = .95)
 
 # 2
+data = rep(c(5, 15, 25), c(7, 15, 3))
+sqrt(sigma.test(data, conf.level = 0.95)$conf)
+result = t.test(data, mu = 13, alternative = "greater")$p.value
+can_reject(result, 0.05)
+
+# 3
+data = c(16,28,32,14,10)
+alpha = .1
+
+sum(data)/5 # można korzystać z testu
+result = chisq.test(data)$p.value
+
+can_reject(result, 0.05)
+# nie jest z równomiernego
+
+# 4
