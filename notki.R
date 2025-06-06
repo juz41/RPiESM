6c3ee9a37ac82e4ba2b93254995523ba24a1558a
juz41 <julek.zalewski@gmail.com>
2025-06-06 20:25:49 +0200

    kolos prep

--- notki.R
+++ notki.R
@@ -29,7 +29,16 @@ alpha = .1
 sum(data)/5 # można korzystać z testu
 result = chisq.test(data)$p.value
 
-can_reject(result, 0.05)
+can_reject(result, alpha)
 # nie jest z równomiernego
 
 # 4
+data = c(1.017, 1.021, 10.015, 1.019, 1.022, 1.019)
+alpha = 0.01
+result = sigma.test(data, conf.level = 1-alpha, alt="greater", sigmasq=0.001)
+can_reject(result$p.value, alpha)
+# nie ma podstaw do odrzucenia H0 - wariancja jest równa 
+
+alpha = 0.05
+sigma.test(data, conf.level=1-alpha)
+
